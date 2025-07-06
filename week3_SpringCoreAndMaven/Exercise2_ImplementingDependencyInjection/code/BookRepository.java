54180266384fff859c8b42119e678411c0cae2fa
Havya Lankireddy <2200031439@kluniversity.in>
2025-07-06 20:34:33 +0530

    week3_springcoreandmaven

---
+++ week3_SpringCoreAndMaven/Exercise2_ImplementingDependencyInjection/code/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.Librarymanagements.repository;
+
+public class BookRepository {
+    public void saveBook(String bookName) {
+        System.out.println("Book saved: " + bookName);
+    }
+}
