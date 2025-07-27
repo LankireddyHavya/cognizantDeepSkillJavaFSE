2f17a091109bd64ef342c9cbf7b3d1d27c4d6cda
Havya Lankireddy <2200031439@kluniversity.in>
2025-07-27 15:37:42 +0530

    week6 ex-4,5

---
+++ week6_React/4. ReactJS-HOL/code/Posts.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      error: null
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ posts: data });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred: " + error.toString());
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
